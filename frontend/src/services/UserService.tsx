e9a1ba96c7377706c50b13cbc417b2923b62e350
Ömer Karaman <omerkaramanx@gmail.com>
2024-01-13 16:21:58 +0300

    services added

---
+++ frontend/src/services/UserService.tsx
@@ -0,0 +1,76 @@
+interface User {
+    id: number;
+    username: string;
+    fullname: string;
+    email: string;
+    birthdate: string;
+  }
+  
+  const BASE_URL = "http://localhost:8000";
+  
+  export const listUsers = async (): Promise<User[]> => {
+    try {
+      const response = await fetch(`${BASE_URL}/listusers`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Kullanıcılar getirilirken hata oluştu:", error);
+      throw error;
+    }
+  };
+  
+  export const addUser = async (newUser: User) => {
+    try {
+      const response = await fetch(`${BASE_URL}/adduser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+        mode: "cors",
+      });
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Kullanıcı eklenirken hata oluştu:", error);
+      throw error;
+    }
+  };
+  
+  export const editUser = async (userId: number, updatedUser: User) => {
+    try {
+      const response = await fetch(`${BASE_URL}/edituser/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedUser),
+        mode: "cors",
+      });
+  
+      if (!response.ok) {
+        console.error("Kullanıcı güncelleme başarısız");
+      }
+    } catch (error) {
+      console.error("Kullanıcı güncellenirken hata oluştu:", error);
+      throw error;
+    }
+  };
+  
+  export const deleteUser = async (userId: number) => {
+    try {
+      const response = await fetch(`${BASE_URL}/deleteuser/${userId}`, {
+        method: "DELETE",
+        mode: "cors",
+      });
+  
+      if (!response.ok) {
+        console.error("Kullanıcıyı silme başarısız oldu");
+      }
+    } catch (error) {
+      console.error("Kullanıcıyı silerken hata oluştu:", error);
+      throw error;
+    }
+  };
+  
