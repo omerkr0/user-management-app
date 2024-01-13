ba851486b6f3387a69beb950d502132c26673659
Ömer Karaman <omerkaramanx@gmail.com>
2024-01-13 19:41:52 +0300

    Comment lines added to handlers

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -11,13 +11,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// AddUserHandler handles the HTTP POST request to add a new user
 func AddUserHandler(rw http.ResponseWriter, req *http.Request) {
-	// Sadece HTTP POST isteklerini kabul et
+	// Only accept HTTP POST requests
 	if req.Method != http.MethodPost {
 		rw.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
+	// Decode the JSON data from the request body into a User struct
 	var newUser database.User
 	decoder := json.NewDecoder(req.Body)
 	if err := decoder.Decode(&newUser); err != nil {
@@ -25,12 +27,14 @@ func AddUserHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// Insert the new user into the database
 	userID, err := database.InsertUser(newUser)
 	if err != nil {
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	// Send a JSON response with the success message and the user ID
 	resp := struct {
 		Message string `json:"message"`
 		UserID  int64  `json:"userID"`
@@ -44,31 +48,35 @@ func AddUserHandler(rw http.ResponseWriter, req *http.Request) {
 	json.NewEncoder(rw).Encode(resp)
 }
 
+// ListUsersHandler handles the HTTP GET request to list all users
 func ListUsersHandler(rw http.ResponseWriter, req *http.Request) {
-	// Sadece HTTP GET isteklerini kabul et
+	// Only accept HTTP GET requests
 	if req.Method != http.MethodGet {
 		rw.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
+	// Retrieve all users from the database
 	users, err := database.GetAllUsers()
 	if err != nil {
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	// Send a JSON response with the list of users
 	rw.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(users)
 }
 
+// EditUserHandler handles the HTTP PUT request to edit an existing user
 func EditUserHandler(rw http.ResponseWriter, req *http.Request) {
-	// Sadece HTTP PUT isteklerini kabul et
+	// Only accept HTTP PUT requests
 	if req.Method != http.MethodPut {
 		rw.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	// URL'den kullanıcı kimliğini al
+	// Extract the user ID from the URL
 	params := mux.Vars(req)
 	userID, err := strconv.Atoi(params["id"])
 	if err != nil {
@@ -77,10 +85,10 @@ func EditUserHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// Güncellenmiş kullanıcı verisini saklamak için bir User struct'ı oluştur
+	// Create a User struct to store the updated user data
 	var updatedUser database.User
 
-	// Gelen JSON verisini çözümle
+	// Decode the JSON data from the request body into the updatedUser struct
 	decoder := json.NewDecoder(req.Body)
 	if err := decoder.Decode(&updatedUser); err != nil {
 		log.Println("Failed to decode user:", err)
@@ -88,25 +96,26 @@ func EditUserHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// Veritabanında kullanıcıyı güncelle
+	// Update the user in the database
 	err = database.UpdateUser(userID, updatedUser)
 	if err != nil {
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	// Başarılı yanıtı gönder
+	// Send a success response
 	rw.WriteHeader(http.StatusOK)
 }
 
+// DeleteUserHandler handles the HTTP DELETE request to delete an existing user
 func DeleteUserHandler(rw http.ResponseWriter, req *http.Request) {
-	// Sadece HTTP DELETE isteklerini kabul et
+	// Only accept HTTP DELETE requests
 	if req.Method != http.MethodDelete {
 		rw.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	// URL'den kullanıcı kimliğini al
+	// Extract the user ID from the URL
 	params := mux.Vars(req)
 	userID, err := strconv.Atoi(params["id"])
 	if err != nil {
@@ -115,13 +124,13 @@ func DeleteUserHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// Veritabanından kullanıcıyı sil
+	// Delete the user from the database
 	err = database.DeleteUser(userID)
 	if err != nil {
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	// Başarılı yanıtı gönder
+	// Send a success response
 	rw.WriteHeader(http.StatusOK)
 }
